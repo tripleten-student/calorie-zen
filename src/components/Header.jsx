c14508c80d23b2ff7619d891d4d311e543bf99f2
Kevin Loughead <kvnloughead@gmail.com>
2024-02-12 15:37:57 -0500

    feat: add image to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import headerLogo from "../assets/calorieZen.png";
+
 function Header() {
   return (
     <div>
+      <img src={headerLogo} alt="Calorie Zen Logo" />
       <h1>Calorie-Zen</h1>
     </div>
   );
